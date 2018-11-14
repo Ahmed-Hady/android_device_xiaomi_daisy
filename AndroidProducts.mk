e295939d79b145e4c7c611b03547284018d616f6
Ahmed Hady <ahmedhady6@gmail.com>
2018-11-15 00:05:18 +0200

    AOSP-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,3 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_daisy.mk
+    $(LOCAL_DIR)/aosp_daisy.mk
 
--- aosp_daisy.mk
+++ aosp_daisy.mk
@@ -2,7 +2,7 @@
 # PRODUCT_RELEASE_NAME := daisy
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+#$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit device configuration
 # $(call inherit-product, device/xiaomi/daisy/full_daisy.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -19,7 +19,7 @@ PRODUCT_MODEL := Mi A2 Lite
 PRODUCT_MANUFACTURER := Xiaomi
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_daisy
+PRODUCT_NAME := aosp_daisy
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
@@ -28,5 +28,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="daisy-user 9.0.0 PIE.1.0 9.0 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/daisy/daisy_sprout:9.0.0/PIE.1.0 9.0/9.0:user/release-keys"
+BUILD_FINGERPRINT := "xiaomi/daisy/daisy_sprout:9.0.0/PIE.1.0-9.0/9.0:user/release-keys"
 
